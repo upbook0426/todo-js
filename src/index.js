524a6d7ada6fde5e64faf94729df09ec75f9e0fd
Upbook <58377749+upbook0426@users.noreply.github.com>
2020-12-10 22:21:31 +0900

    add button

--- src/index.js
+++ src/index.js
@@ -15,22 +15,16 @@ const onClickAdd = () => {
 
   //button(完了)タグ生成
   const completeButton = document.createElement("button");
-  completeButton.innterText ="完了";
-  console.log(completeButton);
+  completeButton.innerText = "完了";
 
-  //button(削除)タグ生成
+  // //button(削除)タグ生成
   const deleteButton = document.createElement("button");
-  deleteButton.innterText ="削除";
-  console.log(deleteButton);
+  deleteButton.innerText = "削除";
 
-   //divタグの子要素に書く要素を設定
-   div.appendChild(li);
-   div.appendChild(completeButton);
-   div.appendChild(deleteButton);
-
-
-  //divタグの子要素に各要素を設定
+  //divタグの子要素に書く要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
